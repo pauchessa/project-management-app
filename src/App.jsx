e36cb8429942a6a878a28653a090bff2859f59e8
pauchessa <lisitsinarenard18@gmail.com>
2026-06-16 16:26:21 +0400

    added form handling with parent state update

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,23 @@ import ProjectsList from "./components/ProjectsList.jsx";
 import NoProjectsPlaceholder from "./components/NoProjectsPlaceholder.jsx";
 import CreateProjectForm from "./components/CreateProjectForm.jsx";
 import ProjectDetails from "./components/ProjectDetails.jsx";
+
+import { useState } from "react";
+
 function App() {
+  const [projectsList, updateProjectsList] = useState([]);
+  console.log(projectsList);
   return (
     <div className="">
       <div className="max-w-7xl mx-auto px-6 py-10 grid grid-cols-[1fr_3fr] min-h-screen">
         <ProjectsList />
         <main className="bg-stone-200">
-          <ProjectDetails />
+          {/* <ProjectDetails /> */}
           {/* <NoProjectsPlaceholder></NoProjectsPlaceholder> */}
-          {/* <CreateProjectForm /> */}
+          <CreateProjectForm
+            projectsList={projectsList}
+            updateProjectsList={updateProjectsList}
+          />
         </main>
       </div>
     </div>
